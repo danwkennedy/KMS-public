17895b35191c49c1e995baa1d75a798615d5ff29
Daniel Kennedy <danwkennedy@gmail.com>
2012-02-19 17:22:21 -0500

    Preliminary push

---
+++ KinectManagementServer/KinectManagementServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using System.Threading;
+
+namespace KinectManagementServer
+{
+    class KinectManagementServer
+    {
+
+        #region Vars
+
+        #endregion
+
+        #region Init
+
+        public KinectManagementServer()
+        {
+            InitKinectClients();
+            InitGesture();
+            InitSocketServer();
+        }
+
+        /// <summary>
+        /// Checks the number of connected sensors and sets up a PipeServer 
+        /// </summary>
+        private void InitKinectClients()
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                foreach (KinectSensor sensor in KinectSensor.KinectSensors)
+                {
+                    if (sensor.Status == KinectStatus.Connected)
+                    {
+                        PipeServer server = new PipeServer("D:\\git\\2KinectTechDemo\\KinectClient\\bin\\Debug\\KinectClient.exe", sensor.UniqueKinectId);
+
+                        Thread kinectThread = new Thread(new ThreadStart(server.ThreadProc));
+                        //threads.Add(kinectThread);
+                        //skeletonDict[sensor.UniqueKinectId] = new Skeleton[2];
+                        kinectThread.Start();
+                    }
+                }
+            }
+        }
+
+        private void InitGesture()
+        {
+
+        }
+
+        private void InitSocketServer()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ KinectManagementServer/Runner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using System.Threading;
+
+namespace KinectManagementServer
+{
+
+    class Runner
+    {
+
+        #region Vars
+
+        #endregion
+
+        #region Init
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private void InitKinectClients()
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                foreach (KinectSensor sensor in KinectSensor.KinectSensors)
+                {
+                    if (sensor.Status == KinectStatus.Connected)
+                    {
+                        PipeServer server = new PipeServer("D:\\git\\2KinectTechDemo\\KinectClient\\bin\\Debug\\KinectClient.exe", sensor.UniqueKinectId);
+
+                        Thread kinectThread = new Thread(new ThreadStart(server.ThreadProc));
+                        //threads.Add(kinectThread);
+                        //skeletonDict[sensor.UniqueKinectId] = new Skeleton[2];
+                        kinectThread.Start();
+                    }
+                }
+            }
+        }
+
+        private void InitGesture()
+        {
+
+        }
+
+        private void InitSocketServer()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
